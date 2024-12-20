b9ee4f46a42119711f6b9ed47cacb5a8801371b2
Priyank Sharma <algo.priyank@gmail.com>
2024-12-21 01:22:11 +0530

    Initial Commit

---
+++ NextSem/NextSemApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextSemApp.swift
+//  NextSem
+//
+//  Created by Priyank Sharma on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NextSemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
